5681378edf5275a759b6e883b1ef5b7a5efd15f8
Aashrayaa <aashu@aashrayaas-MacBook-Air.local>
2026-07-27 22:10:09 +0530

    Add problem description to LeetCode solution

--- LeetCode/01-recyclable-and-low-fat-products.sql
+++ LeetCode/01-recyclable-and-low-fat-products.sql
@@ -1,3 +1,7 @@
+-- Problem: Recyclable and Low Fat Products
+-- Find the IDs of products that are both low fat and recyclable.
+
+-- Solution:
 # Write your MySQL query statement below
 SELECT product_id
 FROM Products
